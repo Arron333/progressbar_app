efc39963339ee48663051e47decc63eb23702463
Your Name <arronpease@gmail.com>
2016-06-27 20:02:39 -0400

    moved classes around and clean up code

--- app/src/main/java/com/example/student/myprogressbar/MainActivity.java
+++ app/src/main/java/com/example/student/myprogressbar/MainActivity.java
@@ -1,10 +1,5 @@
 package com.example.student.myprogressbar;
 
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
 import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
@@ -24,15 +19,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-
-public class AndroidDownloadFileByProgressBarActivity extends Activity {
+public class MainActivity extends Activity {
 
     // button to show progress dialog
     Button btnShowProgress;
 
     // Progress Dialog
     private ProgressDialog pDialog;
-
+    ImageView my_image;
     // Progress dialog type (0 - for Horizontal progress bar)
     public static final int progress_bar_type = 0;
 
@@ -48,7 +42,6 @@ public class AndroidDownloadFileByProgressBarActivity extends Activity {
         btnShowProgress = (Button) findViewById(R.id.btnProgressBar);
         // Image view to show image after downloading
         my_image = (ImageView) findViewById(R.id.my_image);
-
         /**
          * Show Progress bar click event
          * */
@@ -61,13 +54,14 @@ public class AndroidDownloadFileByProgressBarActivity extends Activity {
             }
         });
     }
+
     /**
      * Showing Dialog
      * */
     @Override
     protected Dialog onCreateDialog(int id) {
         switch (id) {
-            case progress_bar_type:
+            case progress_bar_type: // we set this to 0
                 pDialog = new ProgressDialog(this);
                 pDialog.setMessage("Downloading file. Please wait...");
                 pDialog.setIndeterminate(false);
@@ -79,91 +73,93 @@ public class AndroidDownloadFileByProgressBarActivity extends Activity {
             default:
                 return null;
         }
-    }}
-/**
- * Background Async Task to download file
- * */
-class DownloadFileFromURL extends AsyncTask<String, String, String> {
-
-    /**
-     * Before starting background thread
-     * Show Progress Bar Dialog
-     * */
-    @Override
-    protected void onPreExecute() {
-        super.onPreExecute();
-        showDialog(progress_bar_type);
     }
 
     /**
-     * Downloading file in background thread
+     * Background Async Task to download file
      * */
-    @Override
-    protected String doInBackground(String... f_url) {
-        int count;
-        try {
-            URL url = new URL(f_url[0]);
-            URLConnection conection = url.openConnection();
-            conection.connect();
-            // getting file length
-            int lenghtOfFile = conection.getContentLength();
+    class DownloadFileFromURL extends AsyncTask<String, String, String> {
 
-            // input stream to read file - with 8k buffer
-            InputStream input = new BufferedInputStream(url.openStream(), 8192);
-
-            // Output stream to write file
-            OutputStream output = new FileOutputStream("/sdcard/downloadedfile.jpg");
-
-            byte data[] = new byte[1024];
-
-            long total = 0;
-
-            while ((count = input.read(data)) != -1) {
-                total += count;
-                // publishing the progress....
-                // After this onProgressUpdate will be called
-                publishProgress(""+(int)((total*100)/lenghtOfFile));
-
-                // writing data to file
-                output.write(data, 0, count);
-            }
-
-            // flushing output
-            output.flush();
-
-            // closing streams
-            output.close();
-            input.close();
-
-        } catch (Exception e) {
-            Log.e("Error: ", e.getMessage());
+        /**
+         * Before starting background thread
+         * Show Progress Bar Dialog
+         * */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            showDialog(progress_bar_type);
         }
 
-        return null;
+        /**
+         * Downloading file in background thread
+         * */
+        @Override
+        protected String doInBackground(String... f_url) {
+            int count;
+            try {
+                URL url = new URL(f_url[0]);
+                URLConnection conection = url.openConnection();
+                conection.connect();
+                // this will be useful so that you can show a tipical 0-100% progress bar
+                int lenghtOfFile = conection.getContentLength();
+
+                // download the file
+                InputStream input = new BufferedInputStream(url.openStream(), 8192);
+
+                // Output stream
+                OutputStream output = new FileOutputStream("/sdcard/downloadedfile.jpg");
+
+                byte data[] = new byte[1024];
+
+                long total = 0;
+
+                while ((count = input.read(data)) != -1) {
+                    total += count;
+                    // publishing the progress....
+                    // After this onProgressUpdate will be called
+                    publishProgress(""+(int)((total*100)/lenghtOfFile));
+
+                    // writing data to file
+                    output.write(data, 0, count);
+                }
+
+                // flushing output
+                output.flush();
+
+                // closing streams
+                output.close();
+                input.close();
+
+            } catch (Exception e) {
+                Log.e("Error: ", e.getMessage());
+            }
+
+            return null;
+        }
+
+        /**
+         * Updating progress bar
+         * */
+        protected void onProgressUpdate(String... progress) {
+            // setting progress percentage
+            pDialog.setProgress(Integer.parseInt(progress[0]));
+        }
+
+        /**
+         * After completing background task
+         * Dismiss the progress dialog
+         * **/
+        @Override
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog after the file was downloaded
+            dismissDialog(progress_bar_type);
+
+            // Displaying downloaded image into image view
+            // Reading image path from sdcard
+            String imagePath = Environment.getExternalStorageDirectory().toString() + "/downloadedfile.jpg";
+            // setting downloaded into image view
+            my_image.setImageDrawable(Drawable.createFromPath(imagePath));
+        }
+
     }
-
-    /**
-     * Updating progress bar
-     * */
-    protected void onProgressUpdate(String... progress) {
-        // setting progress percentage
-        pDialog.setProgress(Integer.parseInt(progress[0]));
-    }
-
-    /**
-     * After completing background task
-     * Dismiss the progress dialog
-     * **/
-    @Override
-    protected void onPostExecute(String file_url) {
-        // dismiss the dialog after the file was downloaded
-        dismissDialog(progress_bar_type);
-
-        // Displaying downloaded image into image view
-        // Reading image path from sdcard
-        String imagePath = Environment.getExternalStorageDirectory().toString() + "/downloadedfile.jpg";
-        // setting downloaded into image view
-        my_image.setImageDrawable(Drawable.createFromPath(imagePath));
-    }
-
 }
